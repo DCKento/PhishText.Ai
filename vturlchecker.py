0a9ca1fbb6fd4eb29bdf6b8b7ac466ee2b67d68e
DCKento <kento.stewart@gmail.com>
2023-06-11 11:07:20 +1000

    Changes to VT URL checker

--- vturlchecker.py
+++ vturlchecker.py
@@ -1,17 +1,36 @@
-import argparse
 import vt
+import sys
+import time
 
-def scan_url(api_key, url):
-    client = vt.Client(api_key)
-    analysis = client.scan_url(url)
-    client.close()
-    return analysis
+def main():
+    # The URL to analyze is taken from the first argument to the script
+    url_to_analyze = sys.argv[1]
+
+    # Instantiate the client with your API key
+    client = vt.Client('YOUR_API_KEY_HERE')
+
+    try:
+        # Scan the URL
+        analysis = client.scan_url(url_to_analyze)
+
+        # Wait until the analysis is done
+        while True:
+            analysis = client.get_object(f"/analyses/{analysis.id}")
+            if analysis.status == "completed":
+                break
+            time.sleep(30)
+
+        # Get information about the URL
+        url_id = vt.url_id(url_to_analyze)
+        url = client.get_object(f"/urls/{url_id}")
+
+        # Print the number of times the URL was submitted and the last analysis stats
+        print(f"Times submitted: {url.times_submitted}")
+        print(f"Last analysis stats: {url.last_analysis_stats}")
+
+    finally:
+        # Close the client
+        client.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Scan a URL using the VirusTotal API.')
-    parser.add_argument('url', help='The URL to scan.')
-    args = parser.parse_args()
-
-    api_key = '<apikey>'  # Replace '<apikey>' with your actual VirusTotal API key
-    analysis = scan_url(api_key, args.url)
-    print(analysis)
+    main()
