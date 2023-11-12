8cb39e19e77b97c3dab7fa057218c983cb163ce4
Kento <kento.stewart@gmail.com>
2023-11-12 13:56:16 +1100

    Update phishtextAI.py
    Changing to new GPT4 model, adding OTX check

--- phishtextAI.py
+++ phishtextAI.py
@@ -7,6 +7,7 @@ import openai  # Import the OpenAI module
 import requests
 import json
 from flask import Flask, request, render_template
+from OTXv2 import OTXv2 # Import the OTX module
 
 # Function to extract URLs from the SMS message. Uses regex and returns the URL
 def extract_url(sms_text):
@@ -51,15 +52,27 @@ def get_scan_results(api_key, scan_id):
             continue
         return response.json()
 
-# Function to chat with the GPT-3.5-turbo model
-def chat_with_gpt(sms_text, vt_analysis_result, urlscan_analysis_result):
+# Function to check with OTX
+def check_with_alien_vault(url):
+    API_KEY = 'YOUR_ALIENVAULT_OTX_API_KEY'
+    OTX_SERVER = 'https://otx.alienvault.com/'
+    otx = OTXv2(API_KEY, server=OTX_SERVER)
+
+    alerts = otx.get_indicator_details_by_section(OTXv2.IndicatorTypes.URL, url, 'general')
+    if alerts.get('pulse_info', {}).get('count', 0) > 0:
+        return f"Identified as potentially malicious: {json.dumps(alerts)}"
+    else:
+        return 'Unknown or not identified as malicious'
+
+# Function to chat with the GPT-4-turbo model
+def chat_with_gpt(sms_text, vt_analysis_result, urlscan_analysis_result, alien_vault_result):
     openai.api_key = 'OPENAI_API_KEY_HERE'
     messages = [
         {"role": "system", "content": "You are an intelligent assistant that specializes in cybersecurity and the identification and analysis of phishing SMS messages."},
-        {"role": "user", "content": f"Analyze this SMS message: '{sms_text}' and its VirusTotal analysis: '{vt_analysis_result}' and URLScan.io analysis: '{urlscan_analysis_result}' to determine if this is a phishing attempt. Give your reasoning for why this is or is not a phishing SMS"},
+        {"role": "user", "content": f"Analyze this SMS message: '{sms_text}' and its VirusTotal analysis: '{vt_analysis_result}' and URLScan.io analysis: '{urlscan_analysis_result}' AlienVault OTX analysis: '{alien_vault_result}' to determine if this is a phishing attempt. Give your reasoning for why this is or is not a phishing SMS"},
     ]
     
-    model = 'gpt-3.5-turbo'
+    model = 'gpt-4-1106-preview'
     response = openai.ChatCompletion.create(model=model, messages=messages)
 
     # Return the assistant's reply
@@ -76,9 +89,10 @@ def main(sms):
         urlscan_api_key = "URLSCAN_API_KEY_HERE"
         scan_id = submit_scan(urlscan_api_key, url)
         urlscan_analysis_result = None
+        alien_vault_result = check_with_alien_vault(url)
         if scan_id:
             urlscan_analysis_result = get_scan_results(urlscan_api_key, scan_id)
-        chat_gpt_result = chat_with_gpt(sms, vt_analysis_result, urlscan_analysis_result)
+        chat_gpt_result = chat_with_gpt(sms, vt_analysis_result, urlscan_analysis_result, alien_vault_result)
         results.append(chat_gpt_result)
 
     client.close()
